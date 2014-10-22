b1efbed65c3a62fbf102935c06e7bd882c002793
Cherish <shmilycherish@gmail.com>
2014-10-22 09:06:12 +0800

    add view holder and access internet

--- app/src/main/java/com/practice/mydouban/BookListFragment.java
+++ app/src/main/java/com/practice/mydouban/BookListFragment.java
@@ -3,6 +3,7 @@ package com.practice.mydouban;
 import android.app.Fragment;
 import android.content.Context;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +13,8 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RatingBar;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.List;
 
@@ -25,11 +28,21 @@ public class BookListFragment extends Fragment {
     public View onCreateView(final LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_book_list, container, false);
-        final List<Book> books = new Books().getBooks(DataFetcher.readDataFromFile(getActivity()));
 
         final ListView bookList = (ListView) rootView.findViewById(R.id.bookList);
+        new AsyncTask<String, Void, List<Book>>() {
+            @Override
+            protected List<Book> doInBackground(String... params) {
+                String urlStr = params[0];
+                return new Books().getBooks(DataFetcher.readDataFromFile(urlStr));
+            }
 
-        bookList.setAdapter(new MyBookListAdapter(books, getActivity()));
+            @Override
+            protected void onPostExecute(List<Book> books) {
+                super.onPostExecute(books);
+                bookList.setAdapter(new MyBookListAdapter(books, getActivity()));
+            }
+        }.execute("http://api.douban.com/v2/book/search?tag=%E8%AE%A1%E7%AE%97%E6%9C%BA");
         return rootView;
     }
 
@@ -61,20 +74,34 @@ public class BookListFragment extends Fragment {
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            final View bookView = LayoutInflater.from(context).inflate(R.layout.list_item_book, parent, false);
-            final ImageView bookCover = (ImageView) bookView.findViewById(R.id.bookCover);
-            final TextView bookName = (TextView) bookView.findViewById(R.id.bookName);
-            final RatingBar rating = (RatingBar) bookView.findViewById(R.id.rating);
-            final TextView bookInfo = (TextView) bookView.findViewById(R.id.bookInfo);
+            ViewHolder viewHolder;
+            if (convertView == null) {
+                viewHolder = new ViewHolder();
+                convertView = LayoutInflater.from(context).inflate(R.layout.list_item_book, parent, false);
+                viewHolder.bookCover = (ImageView) convertView.findViewById(R.id.bookCover);
+                viewHolder.bookName = (TextView) convertView.findViewById(R.id.bookName);
+                viewHolder.rating = (RatingBar) convertView.findViewById(R.id.rating);
+                viewHolder.bookInfo = (TextView) convertView.findViewById(R.id.bookInfo);
+                convertView.setTag(viewHolder);
+            } else {
+                viewHolder = (ViewHolder) convertView.getTag();
+            }
 
             final Book book = getItem(position);
 
-            bookCover.setImageBitmap(BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_default_cover));
-            bookName.setText(book.getTitle());
-            rating.setRating((float) (book.getRating() / 2));
-            bookInfo.setText(book.getInformation());
-            return bookView;
+            viewHolder.bookCover.setImageBitmap(BitmapFactory.decodeResource(context.getResources(), R.drawable.ic_default_cover));
+            viewHolder.bookName.setText(book.getTitle());
+            viewHolder.rating.setRating((float) (book.getRating() / 2));
+            viewHolder.bookInfo.setText(book.getInformation());
+            return convertView;
         }
+    }
+
+    static class ViewHolder {
+        ImageView bookCover;
+        TextView bookName;
+        RatingBar rating;
+        TextView bookInfo;
     }
 
 }
--- app/src/main/java/com/practice/mydouban/DataFetcher.java
+++ app/src/main/java/com/practice/mydouban/DataFetcher.java
@@ -1,6 +1,5 @@
 package com.practice.mydouban;
 
-import android.content.Context;
 import android.util.Log;
 
 import org.json.JSONException;
@@ -10,23 +9,30 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 
 public class DataFetcher {
 
     private static final String READ_DATA = "readData";
 
-    public static JSONObject readDataFromFile(Context context) {
+    public static JSONObject readDataFromFile(String urlStr) {
 
-        final InputStream inputStream = context.getResources().openRawResource(R.raw.data);
-        final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-        final StringBuffer stringBuffer = new StringBuffer();
-        String line;
         try {
+            URL url = new URL(urlStr);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            final InputStream inputStream = urlConnection.getInputStream();
+            final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            final StringBuffer stringBuffer = new StringBuffer();
+            String line;
             while((line =bufferedReader.readLine()) != null) {
                 stringBuffer.append(line);
             }
             final JSONObject bookData = new JSONObject(stringBuffer.toString());
             Log.d(READ_DATA, bookData.toString());
+            urlConnection.disconnect();
+            inputStream.close();
             return bookData;
         } catch (IOException e) {
             Log.d(READ_DATA, "read error");
